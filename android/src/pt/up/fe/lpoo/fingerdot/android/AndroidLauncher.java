71d9044b7cd03274bb82089b84f030477c79cbc8
Eduardo Almeida <megaedux@me.com>
2014-05-05 17:46:02 +0100

    The WarpClient should now work!

--- android/src/pt/up/fe/lpoo/fingerdot/android/AndroidLauncher.java
+++ android/src/pt/up/fe/lpoo/fingerdot/android/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pt.up.fe.lpoo.fingerdot.FingerDot;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
